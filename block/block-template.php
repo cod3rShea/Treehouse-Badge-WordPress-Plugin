41ca1418051dbf52b79be90ed7e34a6028b57c93
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-11-30 20:18:09 -0500

    start building out structor for user add function to show user information

--- block/block-template.php
+++ block/block-template.php
@@ -4,7 +4,12 @@
 			$what_is_your_treehouse_name = get_field('what_is_your_treehouse_name');
 		?>
 		<div class="treehouse-portfolio-container">
-			<div class="loading-screen"><div></div><div></div><div></div></div>
+			<div class="treehouse-portfolio-user">
+				<div class="loading-screen"><div></div><div></div><div></div></div>
+			</div>
+			<div class="treehouse-portfolio-content">
+				<div class="loading-screen"><div></div><div></div><div></div></div>
+			</div>
 		</div>
 		<script>
 			let userName = "<?php echo $what_is_your_treehouse_name;?>";
@@ -12,13 +17,27 @@
 			jQuery.get( `https://teamtreehouse.com/${userName}.json`, (data) => {
 				console.log('you connected');
 				console.log(data);
+
+				const gatherUser = () => {
+					let userName = data.name;
+					let userImg = data.gravatar_url;
+					let userURL = data.profile_url;
+
+					let html = `<img src="${userImg}"> <a href="${userURL}" target="_BLANK"><h2> ${userName}</h2></a>`;
+					jQuery(".treehouse-portfolio-user").append(html);
+				}
+
 				const gatherPoints = () => {
 					const treeHousePoints = data.points;
+					console.log(treeHousePoints[1]);
 					for (const [key, value] of Object.entries(treeHousePoints).sort(([,a],[,b]) => b-a)) {
 							let html = `<div class="point-portfolio-container"> ${key} ${value} </div>`;
-							jQuery(".treehouse-portfolio-container").append(html);
+							jQuery(".treehouse-portfolio-content").append(html);
 					}
 				}
+
+				jQuery('.loading-screen').remove();
+				gatherUser();
 				gatherPoints();
 
 			}).fail(function() {
