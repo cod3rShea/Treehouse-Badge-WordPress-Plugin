d8ef05644c3f94d21a50ce312973111ca8a070d7
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-11-30 19:38:10 -0500

    remove echo and add var for username

--- block/block-template.php
+++ block/block-template.php
@@ -2,18 +2,21 @@
 	function acf_block_treehouse_portfolio_callback() { ?>
 		<?php 
 			$what_is_your_treehouse_name = get_field('what_is_your_treehouse_name');
-			echo $what_is_your_treehouse_name;
 		?>
 		<div class="treehouse-portfolio-container">
 			<div class="loading-screen"><div></div><div></div><div></div></div>
 		</div>
 		<script>
+			let userName = "<?php echo $what_is_your_treehouse_name;?>";
+
 			jQuery.get( `https://teamtreehouse.com/${userName}.json`, (data) => {
 				console.log('you connected');
 				console.log(data);
+
 			}).fail(function() {
 				throw Error( "error" );
 			});
+
 		</script>
 	<?php } 
 ?>
